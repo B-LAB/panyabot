0c144d1ca5b28316b96dc0653298066814c226f0
wasch <w.ndaiga@gmail.com>
2015-10-04 13:33:36 +0300

    bluetooth search and device match

--- app/rfcommlin.sh
+++ app/rfcommlin.sh
@@ -3,8 +3,21 @@ args=("$@")
 # $@ is a special array used to store bash command line arguments
 # you can access these args using this format: ${arg[x]} with zero indexing
 echo "Setting up RFCOMM bind for" ${args[0]} "on *NIX host"
-hcitool name ${args[0]}
-rfcomm release ${args[0]}
-echo 1234 | bluez-simple-agent hci0 ${args[0]}
-rfcomm bind /dev/rfcomm0 ${args[0]} 1
-rfcomm
+hciconfig hci0 up
+
+devfind=$(grep -q $args[0]} <<< $(rfcomm))
+if l2ping ${args[0]} -c 1; then
+	rfport=$(grep -o 'rfcomm.' <<< $(rfcomm))
+	if $devfind; then
+		echo "Found device at" $rfport
+		rfcomm release /dev/$rfport
+		rfcomm bind /dev/$rfport ${args[0]} 1
+		echo $(hcitool name ${args[0]}) "already bound!"
+	else
+		echo 1234 | bluez-simple-agent hci0 ${args[0]}
+		rfcomm bind /dev/$rfport ${args[0]} 1
+	fi
+	rfcomm
+else
+	echo ${args[0]} "not found!"
+fi
