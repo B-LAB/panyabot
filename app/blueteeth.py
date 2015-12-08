e6bc178ef628f21513d6be00eeb43407d8d32115
BLAB <buildlab@BuildLabs-Mac-mini.local>
2015-12-08 17:15:52 +0300

    code commenting added to blueteeth

--- app/blueteeth.py
+++ app/blueteeth.py
@@ -19,11 +19,17 @@ from sys import platform as _platform
 # else:
 # 	blescan = False
 
+# import filepaths from config file
 bdir = app.config["BASE"]
 sdir = app.config["DATA"]
+# search result from bluetooth legacy discovery stored in resp list
 resp = []
+# bluetooth pairing flush flag
 reset=""
 
+# determine which platform this app package is running on and set the
+# required shell/bash script paths. these scripts manage all the
+# hosts bluetooth pairing and device tree attaching with the robots.
 if _platform == "linux" or _platform == "linux2":
 	rfpath = os.path.join(bdir,"app","rfcommlin.sh")
 	host="lin"
@@ -32,6 +38,8 @@ else:
 	host="win"
 
 def leginquire():
+	# bluetooth legacy discovery api endpoint. This endpoint is used by the
+	# registration page when searching for nearby bluetooth devices.
 	global resp
 	global i
 	resp = []
@@ -41,14 +49,21 @@ def leginquire():
 			resp.append({'mac':str(addr),'name':str(name)})
 		except UnicodeEncodeError:
 			resp.append({'mac':str(addr),'name':str(name.encode('utf-8', 'replace'))})
+	
+	# ble discovery endpoint. This trigger is a work in progress due to limitations
+	# with the pybluez library.
 	# if ((resp == []) & (blescan)):
 		# bleinquire()
+
+	# jsonify the bluetooth search results to be consumed by the 
+	# registration page ajax pull request.
 	response = json.dumps(resp)
 	return response
 
-def sdpbrowse(uid=None):
-	target = uid
-	print target
+def sdpbrowse(macid=None):
+	# this function determines the available service profiles at the specified bluetooth macid.
+	target = macid
+	print "Determining service profiles at "+str(target)
 	if target == "all": target = None
 
 	services = bluetooth.find_service(address=target)
@@ -72,6 +87,12 @@ def sdpbrowse(uid=None):
 	    print()
 
 def rfcommbind(rfcset,macid,alias=None,unick=None,commands=None,uid=None,rst=None):
+	# this function takes the supplied macid passing it to the bash/shell script to
+	# pair to using the simple-bluez-agent tool and attach said macid to a 
+	# /dev/rfcomm port. If the reset flag is passed, the associated robot macid is 
+	# flushed i.e. released from rfcomm and the pairing entry deleted from 
+	# /var/lib/bluetooth/{local host macid}/linkkeys. This flushing might be overkill
+	# but it ensures that all host-robot sessions are handled robustly.
 	global reset
 	if rst is None:
 		try:
@@ -87,7 +108,11 @@ def rfcommbind(rfcset,macid,alias=None,unick=None,commands=None,uid=None,rst=Non
 				print '********************************************************************'
 			datasend(macid,alias,unick,commands,rfcset,uid)
 		except Exception,e:
+			# if an exception is caught while pairing or dev attaching, the associated
+			# robot and macid are flushed to maintain database and device listing integrity.
 			robot = Robot.query.filter_by(user_id=uid).first()
+			# the associated robot status is set to inactive to allow queued connections to
+			# be handled.
 			robot.status="inactive"
 			db.session.commit()
 			print "Error Binding RFCOMM Device"
@@ -119,6 +144,8 @@ def rfcommbind(rfcset,macid,alias=None,unick=None,commands=None,uid=None,rst=Non
 			for rob in robots:
 				print "%s:%s" %(robot.alias,robot.status)
 		except Exception,e:
+			# if an error occurs while trying to flush and no ORM-related error is provided
+			# a error state with the subprocess call can be assumed.
 			print "Error Releasing RFCOMM device!"
 			robot = Robot.query.filter_by(user_id=uid).first()
 			robot.status="inactive"
@@ -126,6 +153,12 @@ def rfcommbind(rfcset,macid,alias=None,unick=None,commands=None,uid=None,rst=Non
 			print str(e)
 
 def datasend(macid,alias,unick,commands,rfcset,uid):
+	# this is the command transport mechanism. A serial port is opened at the rfcset
+	# declared devport and commands transmitted using the pyserial library.
+	# currently in testing, default preset values are sent to the attached robot that
+	# must be running the arduino panyabot sketch.
+	# future feature to use the standard firmata library to have bidirectional
+	# transmission of data (commands and sensor values).
 	global reset
 	devport = "/dev/"
 	devport+=str(rfcset)
@@ -142,11 +175,16 @@ def datasend(macid,alias,unick,commands,rfcset,uid):
 	ser.write('1')
 	ser.close()
 	reset = "y"
+	# print the stored commands to the terminal window
 	for i in range(0,len(commands)):
 		print commands[i]
+	# after downstream data transmission is completed, the attached robot is flushed.
 	rfcommbind(rfcset,macid,None,None,None,uid,reset)
 
 def rfcommset(robots):
+	# this function manages the allocation of rfcomm port numbers to each incoming request.
+	# prstlist stores the previously allocated dev numbers that haven't been declared inactive.
+	# prstflag indicates that the function must iterate to the lowest unused port number.
 	prstlist={}
 	prstflag=False
 	for robot in robots:
@@ -158,23 +196,34 @@ def rfcommset(robots):
 				prstlist['robot.alias']=devno
 				prstflag=True
 			else:
+				# if an integrity error is found, where the robots status value and the
+				# regex match for the rfcomm port number don't associate in the expected way,
+				# then the robots status value is forcefully reset to inactive.
 				print 'Possible error with status setting for %s' %(robot.alias)
 				print 'Resetting status value to inactive.'
 				robot.status="inactive"
 				db.session.commit()
 				prstflag=False
+
 	if prstflag:
+		# this conditional and nested loop interate over the values stored
+		# in the prstlist to determine what port value to assign for the current
+		# request.
 		for key, value in prstlist.iteritems():
 			maxval=value
 			if value>maxval:
 				maxval=value
 		setval=maxval+1
 	else:
+		# if prstflag is not true, default to assign at port 0 (i.e. /dev/rfcomm0)
 		setval=0
 	setcomm="rfcomm"+str(setval)
 	return setcomm
 
 def portsetup(commands):
+	# this function checks if any current robots are attached and in process
+	# if there are, the current request is queued until such a time that all
+	# prior tranmissions are completed.
 	Qflag = False
 	user = User.query.filter_by(nickname=g.user.nickname).first()
 	robot = Robot.query.filter_by(user_id=user.id).first()
@@ -185,8 +234,7 @@ def portsetup(commands):
 			print "Queuing bluetooth upload"
 			Qflag = True
 			rfcset = "NULL"
-			# a procedure to manage concurrent bluetooth requests
-			# should come here
+			# a procedure to manage concurrent bluetooth requests should come here
 			Qflag = False
 	if not Qflag:
 		# rfcset = "rfcomm0"
@@ -199,6 +247,8 @@ def portsetup(commands):
 	# using a module that does. Bug number will be attached to this issue.
 
 def parseblocks(code):
+	# this is where blockly code is parsed into a python file with the command list
+	# saved in memory for transimission.
 	response = json.dumps(code)
 	t = datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
 	savedir = os.path.join(sdir, g.user.nickname)
