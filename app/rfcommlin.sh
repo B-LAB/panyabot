f5be2ec108c8d89b9cb49d388b9f683ba37e71d4
BLAB <buildlab@BuildLabs-Mac-mini.local>
2015-12-15 16:31:50 +0300

    general commenting

--- app/rfcommlin.sh
+++ app/rfcommlin.sh
@@ -4,25 +4,41 @@ args=("$@")
 # you can access these args using this format: ${arg[x]} with zero indexing
 hcino=$(grep -o "hci." <<< $(hciconfig))
 hcist=$(grep -o "down" <<< $(hciconfig))
+
+# conditional to check that the hci device is not down
+# -z is a bash empty variable check.
 if [ -z "$hcist"]; then
 	hciconfig $hcino up
 fi
 
+# conditional to determine if the reset flag has been set. if true, passed macid
+# is flushed; if false, passed macid is paired to and bound to given rfcomm port.
 if [ -z "${args[2]}" ]; then
+	# restart systemd dbus and bluetooth services as a fail safe check
 	service dbus restart
 	service bluetooth restart
+	# double check the hci device, probably could do with less checks, but just to
+	# ensure reliable operation
 	if [ -z "$hcist"]; then
 		hciconfig $hcino up
 	fi
 	echo "Setting up RFCOMM bind for" ${args[0]} "on *NIX host"
-	# check if submitted uid-attached host is up
+	
+	# bluetooth ping(ONCE) the passed macid variable to confirm it's up
 	if l2ping ${args[0]} -c 1; then
-		# devfind checks if submitted UID is already registered on rfcomm
+		# devfind conditional checks if submitted UID is already registered on rfcomm,
+		# if not it pairs to and binds the passed macid variable
 		devfind=$(grep -o ${args[0]} <<< $(rfcomm))
 		if [ -z "$devfind" ]; then
+			# pair to the passed macid variable.
+			# NOTE: Perhaps I should check linkkeys if device has already been
+			# paired to?
 			echo 1234 | bluez-simple-agent $hcino ${args[0]}
+			# rfchck conditional checks if the passed macid variable has already been
+			# bound. It releases and binds the passed macid if it has.
 			rfchck=$(grep -o ${args[1]} <<< $(rfcomm))
 			if [ -z "$rfchck" ]; then
+				# bind the passed macid to the assigned rfcomm port on channel 1
 				rfcomm bind "/dev/"${args[1]} ${args[0]} 1
 			else
 				rfcomm release "/dev/"${args[1]}
@@ -30,18 +46,20 @@ if [ -z "${args[2]}" ]; then
 			fi
 			echo "rfport:" ${args[1]}
 		else
-			# rfport searches for the attached /dev/rfcomm{x} device
+			# rfport searches for the bound rfcomm port number e.g. /dev/rfcomm(?)
 			echo $(hcitool name ${args[0]}) "already bound"
 			rfport=$(grep -o 'rfcomm.' <<< $(rfcomm))
 			echo "rfport: /dev/"$rfport
 		fi
 		rfcomm
 	else
+		# bluetooth ping failed to find passed macid
 		rfport="NULL"
 		echo "rfport:" $rfport
 		echo ${args[0]} "not found"
 	fi
 else
+	# conditionals that ensure robust flushing if errors are found
 	echo "Setting up RFCOMM release for" ${args[0]} "on *NIX host"
 	rfchck=$(grep -o ${args[1]} <<< $(rfcomm))
 	if [ -z "$rfchck" ]; then
--- db_create.py
+++ db_create.py
@@ -4,14 +4,21 @@ from app import db, app
 import os.path
 
 def setdb():
-	# This function creates an app database
-	# and the migration data repository
+	# This function creates an app database and the migration data repository
+	
+	# these two variables are required by the sqlalchemy library and point to
+	# the save directories in which the db and migration repository will be
+	# stored.
 	SQLALCHEMY_DATABASE_URI=app.config['SQLALCHEMY_DATABASE_URI']
 	SQLALCHEMY_MIGRATE_REPO=app.config['SQLALCHEMY_MIGRATE_REPO']
+	
+	# db creation comes first
 	try:
 		db.create_all()
 	except:
 		print 'Error creating app database'
+	
+	# followed by the migration repository
 	try:
 		if not os.path.exists(SQLALCHEMY_MIGRATE_REPO):
 		    api.create(SQLALCHEMY_MIGRATE_REPO, 'database repository')
@@ -22,6 +29,7 @@ def setdb():
 		print 'Error creating migration data repository'
 
 def main():
+	# this function call will be removed in production code
 	setdb()
 
 if __name__ == "__main__":
